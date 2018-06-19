4bc201b241f813bcae6171d8b23e7966bfa887d5
Michael Scarpace <mscarpace@deloitte.com>
2018-06-19 16:00:33 -0400

    adding commands to DBACommands

--- src/commands/DBACommands.java
+++ src/commands/DBACommands.java
@@ -7,6 +7,7 @@ public class DBACommands {
 		System.out.println("GRANT ALL PRIVILEGES ON *.* TO \'tolkien\'@\'%\';");
 		System.out.println("Show all databases");
 		System.out.println("Show all tables");
+		System.out.println("To connect using mysql shell: "+"\\connect mscarpace:password@localhost:3306/addressbook");
 	}
 
 }
