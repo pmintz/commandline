3b4ba5ef04a8e19032802e139d953ddd8136672e
Michael Scarpace <mscarpace@deloitte.com>
2018-06-19 15:30:27 -0400

    adding ShowAllCommands and updating DBACommnads

--- src/commands/DBACommands.java
+++ src/commands/DBACommands.java
@@ -5,7 +5,8 @@ public class DBACommands {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GRANT ALL PRIVILEGES ON *.* TO \'tolkien\'@\'%\';");
-
+		System.out.println("Show all databases");
+		System.out.println("Show all tables");
 	}
 
 }
---
+++ src/commands/ShowAllCommands.java
@@ -0,0 +1,16 @@
+package commands;
+
+public class ShowAllCommands {
+
+	public static void main(String [] args){
+		System.out.println("commands.CheckClassPath");
+		System.out.println("commands.CheckPath");
+		System.out.println("commands.Classpath");
+		System.out.println("commands.DBACommands");
+		System.out.println("commands.GitCommands");
+		System.out.println("commands.LinuxCommands");
+		System.out.println("commands.Path");
+		System.out.println("commands.ShowAllCommands");
+		System.out.println("commands.WindowsCommands");
+	}
+}
