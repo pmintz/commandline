2239f4ac6e44d0a15db9299a3581e97a3f9e57ef
Michael Scarpace <mscarpace@deloitte.com>
2018-09-04 12:36:09 -0400

    adding informational console messages to DBACommands

--- src/commands/DBACommands.java
+++ src/commands/DBACommands.java
@@ -5,6 +5,8 @@ public class DBACommands {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GRANT ALL PRIVILEGES ON *.* TO \'tolkien\'@\'%\';");
+		System.out.println("*.* = alldatabase.alltables");
+		System.out.println("% = any valid host (in place of localhost or specific host) ");
 		System.out.println("Show all databases");
 		System.out.println("Show all tables");
 		System.out.println("To connect using mysql shell: "+"\\connect mscarpace:password@localhost:3306/addressbook");
