7f48ebeec2baeec6f8abe0a948d19b28b0e1846d
Michael Scarpace <mscarpace@deloitte.com>
2018-09-05 18:00:30 -0400

    updating DBACommands

--- src/commands/DBACommands.java
+++ src/commands/DBACommands.java
@@ -10,6 +10,16 @@ public class DBACommands {
 		System.out.println("Show all databases");
 		System.out.println("Show all tables");
 		System.out.println("To connect using mysql shell: "+"\\connect mscarpace:password@localhost:3306/addressbook");
+		System.out.println("*******************");
+		System.out.println("lsnrctl commands");
+		System.out.println("lsnrctl start");
+		System.out.println("lsnrctl stop");
+		System.out.println("lsnrctl reload");
+		System.out.println("*********************");
+		System.out.println("admin commands");
+		System.out.println("sqlplus /nolog");
+		System.out.println("connect /as sysdba");
+		System.out.println("ALTER USER sys IDENTIFIED BY new_password;");
 	}
 
 }
