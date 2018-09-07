280d8794b43adf2e92846420cd2ccd7b564620fa
Michael Scarpace <mscarpace@deloitte.com>
2018-09-07 15:18:27 -0400

    adding git commands

--- src/commands/GitCommands.java
+++ src/commands/GitCommands.java
@@ -3,17 +3,21 @@ package commands;
 public class GitCommands {
 	
 	public static void main(String []agrs){
-	
+	System.out.println("******************");
 	System.out.println("push existing repo");
 	System.out.println("git init");
 	System.out.println("git remote add origin https://github.com/pmintz/katalon.git");
 	System.out.println("git push -u origin master");
-	System.out.println("create new rep");
+	System.out.println("*******************");
+	System.out.println("create new repo");
 	System.out.println("git init");
 	System.out.println("git add README.md");
 	System.out.println("git commit -m \"first commit\"");
 	System.out.println("git remote add origin https://github.com/pmintz/katalon.git");
 	System.out.println("git push -u origin master");
+	System.out.println("*******************");
+	System.out.println("remove git from local repo");
+	System.out.println("rm -rf .git");
 	
 	}
 
