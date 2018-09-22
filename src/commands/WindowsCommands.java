fe0e765993f9c4589d777396121c2e876bfce9a7
Michael Scarpace <mscarpace@deloitte.com>
2018-09-22 17:47:11 -0400

    updating WindowsCommands

--- src/commands/WindowsCommands.java
+++ src/commands/WindowsCommands.java
@@ -1,5 +1,14 @@
 package commands;
 
 public class WindowsCommands {
+	
+	
+	public static void main(String []args){
+	System.out.println("***Removing a file that displays \'Access Denied \'***");
+	System.out.println("With commandline prompt open, open task manager and end explorer.exe");
+	System.out.println("Return to prompt and type del /f \'file\'");
+	System.out.println("********************************************************************");
+	}
+	
 
 }
